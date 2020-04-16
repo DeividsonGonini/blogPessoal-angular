558b1491df9ca1d6f8712d0df296552d33baf4e3
DeividsonGonini <deividsongonini@gmail.com>
2020-04-16 00:32:44 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //CRUD - Create, Ready, Update e Delete.
+
+  //Read - listara todas as postagens
+  getAllPostagens() {
+    // colocar o ip do servidor
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
 }
